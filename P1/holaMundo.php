99e3bf7f9d0420a6cbab344d6bd27d079bfa963d
Marius Mladinovici <al315550@uji.es>
2019-10-03 21:37:46 +0200

    Modificado fichero registerForm para tener más campos en el formulario

--- P1/holaMundo.php
+++ P1/holaMundo.php
@@ -1,15 +1,10 @@
 <?php
 
 /**
- * * Descripción: Hola Mundo PHP
- * *
- * * Descripción extensa: Iremos añadiendo cosas complejas en PHP.
- * *
- * * @author  Lola <dllido@uji.es> <fulanito@example.com>
- * * @copyright 2017 Lola
- * * @license http://www.fsf.org/licensing/licenses/gpl.txt GPL 2 or later
- * * @version 1
- * */
+ * @title: registerForm.php
+ * @author: Marius Mladinovici Danaila <al315550@uji.es>
+ * @license CC-BY-NC-SA
+ */
 
 $nombre = "Ana";
 
